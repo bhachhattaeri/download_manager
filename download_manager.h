6e679a13f4e95cdc0aa1b38256fd5118a2af0a40
limjingrong <limjingrong@users.noreply.github.com>
2017-03-16 18:17:15 -0500

    Update download_manager.h

--- download_manager.h
+++ download_manager.h
@@ -1,16 +1,17 @@
-<<<<<<< HEAD
-void download(char* div, char url);
-=======
-#include <unistd.h>
-#include <string.h>
-#include <magic.h>
-#include <stdio.h>
-#include <sys/stat.h> 
-#include <fcntl.h>
-#include <sys/types.h>
-#include <dirent.h>
-#include <curl/curl.h>
-#include <stdlib.h>
 
-void download(char * dir, char * url);
->>>>>>> 214e40867cafa0c755663050df0514f5592c7b53
+
+void download(char * dir, char * url){
+	CURL * c;
+ 	curl_global_init(CURL_GLOBAL_DEFAULT);		//sets up the required environment variables
+  	c = curl_easy_init();		
+  	FILE * f = fopen(dir, "w+");
+   	if(c){
+   		curl_easy_setopt(c, CURLOPT_URL, url);			// uses the url to retrieve info
+   		curl_easy_setopt(c, CURLOPT_VERBOSE, 1);		// provides meaningful message to the user
+   		curl_easy_setopt(c, CURLOPT_WRITEDATA, f);		// stored the retrieved file in that location
+   		curl_easy_perform(c);
+
+		curl_easy_cleanup(c);
+	}else perror(stderr,"Oops...Something went wrong. Please try again.\n");
+	fclose(f);
+}
