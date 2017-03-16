214e40867cafa0c755663050df0514f5592c7b53
MohmmedIshaque <mashaik2@illinois.edu>
2017-03-16 18:06:48 -0500

    Add files via upload

--- daemon.c
+++ daemon.c
@@ -9,14 +9,11 @@
 #include <signal.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/un.h>
+#include <netinet/in.h>
 
-void kill_parent(pid_t id){
-	kill(SIGTERM,id);
-}
-//What is a session leader?
-//What is a group leader?
-
-
+#define _GNU_SOURCE
 int main(){
     pid_t sid = 0;
 	printf("My id is %d and my parent's Process id is %d.\n",getpid(),getppid());
@@ -25,31 +22,65 @@ int main(){
 	
 	if(child > 0){
 		//parent process
-		printf("My id is %d and My parent's Process id is %d.\n",getpid(),getppid());
-		kill_parent(getpid());
+		exit(EXIT_SUCCESS);
 	}
 	else if(child < 0){
-	    //fork failled.print error message
 		perror("Download Manager failed.\n");
 	}
-	else{
-		//fork successful. We created a child process. Once the main process ends, the child process will become daemon.
-		
-            chdir("/");
-		    printf("My id is %d and My parent's Process id is %d.\n",getpid(),getppid());
+	else{	
+  			umask(0);
+
+			pid_t sid;
+			sid = setsid();
+			if(sid < 0){
+				exit(EXIT_FAILURE);
+			}
+            
+			if(chdir("/") < 0){
+				exit(EXIT_FAILURE);
+			}
+
 			close(STDIN_FILENO);
 			close(STDOUT_FILENO);
 			close(STDERR_FILENO);
-			free(command);
-			command = NULL;
+
+			//Daemon is created by the above codes. The following codes should read from the incoming TCP connection and do appropriate procedure.
 
 			while(1){
-				int server_sock = socket(AF_LOCAL,SOCK_STREAM,0);			
+				int server_sock = socket(AF_UNIX,SOCK_STREAM,0);
+				if(server_sock==-1){
+					perror("The file can not be downloaded.\n");
+					continue;
+				}
+
+				struct sockaddr_in  server_addr;
+				server_addr.sin_family = AF_UNIX;
+				server_addr.sin_port = htons(2017);
+				(server_addr.sin_addr).s_addr = htons(INADDR_ANY);
 				
+				int is_sock_bind = bind(server_sock,(struct sockaddr*)&(server_addr.sin_addr), sizeof(struct sockaddr_un));
+				if(is_sock_bind == -1){
+					perror("The file can not be downloaded.\n");
+					continue;
+				}
+
+				int can_listen = listen(server_sock,128);
+				if(can_listen == -1){
+					perror("The file can not be downloaded.\n");
+					continue;
+				}
+
+				struct sockaddr *incoming = NULL;
+				socklen_t addr_len = sizeof(struct sockaddr_un);
+				int is_accepted = accept(server_sock, incoming, &addr_len);
+
+				if(is_accepted < 0){
+					perror("The file can not be downloaded.\n");
+					continue;
+				}
+
 			}
 		}
 	
 	return 0;
 }
-
-}i
