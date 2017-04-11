2be541e0efc89c2a507a566ec8d48a610a324132
limjingrong <limjingrong@users.noreply.github.com>
2017-04-11 10:20:47 -0500

    Update download_manager.c

--- download_manager.c
+++ download_manager.c
@@ -17,11 +17,11 @@ void * download(void * obj){
 	char * url = file->url;
 	CURL * c;
  	curl_global_init(CURL_GLOBAL_DEFAULT);		//sets up the required environment variables
-  	c = curl_easy_init();		
-  	FILE * download_dir = fopen(dir, "w+");
-  	CURLcode success;
-  	int exit_succ = 0;
-   	if(c){
+  c = curl_easy_init();		
+  FILE * download_dir = fopen(dir, "w+");
+  CURLcode success;
+  int exit_succ = 0;
+  if(c){
    		curl_easy_setopt(c, CURLOPT_URL, url);			// uses the url to retrieve info
    		curl_easy_setopt(c, CURLOPT_VERBOSE, 1);		// provides meaningful message to the user
    		curl_easy_setopt(c, CURLOPT_WRITEDATA, download_dir);		// stored the retrieved file in that location
@@ -48,30 +48,40 @@ void * download(void * obj){
 	else return (void*)-1;
 }
 
-void download_wrapper(char * dir[], char * url[], int total_files){
+void download_wrapper(char * dir[], char * url[], char ** times){
+  printf("Received the call\n" );
+  /*
+    CURRENTLY NOT MULTI THREADED BECAUSE
+    PTHREAD_CREATE DOES NOT WORK
+    ONCE WE GET EVERYTHING TO WORK
+    ADDDING THIS FUNCTIONALITY WILL
+    NOT BE DIFFICULT
+  */
+
 	int idx = 0;
-	pthread_t threads[total_files];
-	download_obj files[total_files];
+	pthread_t threads[1024];
+	download_obj files[1024];
 	while(url[idx]){
-		files[idx].dir = strdup(dir[idx]);
-		files[idx].url = strdup(url[idx]);
+	  files[idx].dir = strdup(dir[idx]);
+	  files[idx].url = strdup(url[idx]);
 		pthread_create(&threads[idx],NULL,download,(void*)&files[idx]);
 		idx++;
 	}
-	idx = 0;
+ // download(&files[0]);
+	idx = idx-1;
 	void * ret;
-	while(idx<total_files){
+	while(idx>=0){
 		pthread_join(threads[idx],&ret);
-		idx++;
+		idx--;
 	}
 
 }	
 void check_for_updates(char * url, long prev_mod, char * download_dir){
 	CURL * curl;
  	curl_global_init(CURL_GLOBAL_DEFAULT);		//sets up the required environment variables
-  	curl = curl_easy_init();
-  	curl_easy_setopt(curl, CURLOPT_URL, url);	
-    curl_easy_setopt(curl, CURLOPT_NOBODY, 1L);			// don't get us the download file - we just need to know modification time
+  curl = curl_easy_init();
+  curl_easy_setopt(curl, CURLOPT_URL, url);	
+  curl_easy_setopt(curl, CURLOPT_NOBODY, 1L);			// don't get us the download file - we just need to know modification time
     curl_easy_setopt(curl, CURLOPT_FILETIME, 1L);
    	CURLcode curl_success = curl_easy_perform(curl);	
    	long time = 0;
@@ -79,6 +89,9 @@ void check_for_updates(char * url, long prev_mod, char * download_dir){
    	  curl_success = curl_easy_getinfo(curl, CURLINFO_FILETIME, &time);		// retrieves the information
       if(CURLE_OK == curl_success) {
         printf("Modification time %s",ctime(&time));				/* prints out time in readable format */
+      }else{
+         perror("Ummm....something went wrong. Please try again.\n");
+         exit(EXIT_FAILURE);
       }
       if(prev_mod<time){
       	 download_obj obj;
@@ -87,14 +100,21 @@ void check_for_updates(char * url, long prev_mod, char * download_dir){
 
       	 download(&obj);						// the file has been updated since, so download new copy
    	  }else{
-   		   perror("Ummm....something went wrong. Please try again.\n");
-   		   exit(EXIT_FAILURE);
+   		   fprintf(stdout, "The file is up-to-date!\n");
+         //perror("Ummm....something went wrong. Please try again.\n");
+   		   exit(EXIT_SUCCESS);
    	  }
    	exit(EXIT_SUCCESS);
   }
 }  
 
- /*
- int main(){
- 	check_for_updates("http://www.marxistsfr.org/ebooks/lenin/state-and-revolution.pdf");
- }*/
+ 
+ // int main(){
+ //    char * url[2];
+ //    url[0] = "http://www.marxistsfr.org/ebooks/lenin/state-and-revolution.pdf";
+ //    url[1] = NULL;
+ //    char * dir[2];
+ //    dir[0] = "/Users/Bhawesh/GitHub/donwload_manager/state_AAAA";
+ //    dir[1] = NULL;
+ //    download_wrapper(dir,url,1);
+ // }
